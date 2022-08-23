1cdf07be700ab7d54347593722d7fda17347fc56
vmalwade88 <111086311+vmalwade88@users.noreply.github.com>
2022-08-23 10:07:49 +0530

    Update site.pp
    updated

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,4 @@
 file { '/root/README1':
     ensure => file,
-    content => 'this is my first readme file',
+    content => 'this is my first readme file, created from github and deploy on puppet server',
 }
